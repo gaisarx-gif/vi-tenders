f2e80ecc3070f5a552fd5fdf3040772a8e7c7b89
gaisarx-gif <gaisarx-gif@users.noreply.github.com>
2026-05-20 19:50:52 +0300

    debug: test with helmet inlcuded

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import helmet from 'helmet';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { logger } from '../server/lib/logger.js';
 
-logger.info('Testing cors + http-proxy-middleware...');
+logger.info('Testing with helmet included...');
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
